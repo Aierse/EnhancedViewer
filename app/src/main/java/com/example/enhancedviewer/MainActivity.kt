f440490d64f78a002d8a1daf098c8e9d3286f5b9
Aierse <dbsgur2008@gmail.com>
2022-06-09 21:20:00 +0900

    정리 - 기본 틀

--- app/src/main/java/com/example/enhancedviewer/MainActivity.kt
+++ app/src/main/java/com/example/enhancedviewer/MainActivity.kt
@@ -1,12 +1,7 @@
 package com.example.enhancedviewer
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
 import com.example.enhancedviewer.databinding.ActivityMainBinding
@@ -24,17 +19,24 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
-            R.id.action_settings -> true
+            R.id.bookmark -> {
+                true
+            }
+            R.id.bookmarkAdd -> {
+                true
+            }
+            R.id.search -> {
+                true
+            }
+            R.id.fontSizeSetting -> {
+                true
+            }
             else -> super.onOptionsItemSelected(item)
         }
     }
