68906770ed8e7cd26cb77292d7fafffcf08a0ae6
Aierse <dbsgur2008@gmail.com>
2022-06-12 16:24:00 +0900

    기능 - 북마크 데이터 불러오기

--- app/src/main/java/com/example/enhancedviewer/MainActivity.kt
+++ app/src/main/java/com/example/enhancedviewer/MainActivity.kt
@@ -7,6 +7,7 @@ import android.net.Uri
 import android.os.Bundle
 import android.provider.OpenableColumns
 import android.text.InputType
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
@@ -18,8 +19,10 @@ import androidx.recyclerview.widget.LinearSmoothScroller
 import androidx.recyclerview.widget.RecyclerView
 import com.example.enhancedviewer.databinding.ActivityMainBinding
 import java.io.BufferedReader
+import java.io.File
 import java.io.IOException
 import java.io.InputStreamReader
+import kotlin.streams.toList
 
 class MainActivity : AppCompatActivity() {
     private val OPEN_REQUEST_CODE = 41
@@ -111,6 +114,17 @@ class MainActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             R.id.bookmark -> {
+                val fileName = title.toString()
+
+                if (File("$filesDir/$fileName").exists()) {
+                    val bookmarkList = openFileInput(fileName).bufferedReader().lines().toList()
+
+                    for (i in bookmarkList) {
+
+                        Log.d("tttt", i)
+                    }
+                }
+
                 true
             }
             R.id.bookmarkAdd -> {
@@ -124,45 +138,14 @@ class MainActivity : AppCompatActivity() {
                     setMessage(data[position])
 
                     setPositiveButton("확인") { dialog, which ->
-                        try {
-                            val os = openFileOutput(fileName, MODE_APPEND)
-
-                            os.write(("$fileName,$position\n").toByteArray())
+                        openFileOutput(fileName, MODE_APPEND).use {
+                            it.write(("$fileName,$position\n").toByteArray())
                             Toast.makeText(baseContext, "북마크가 저장되었습니다.", Toast.LENGTH_SHORT).show()
 
-                            os.close()
-                        } catch (e: IOException) {
-
+                            it.close()
                         }
                     }
-                    setNegativeButton("취소") { dialog, which ->
-                        dialog.dismiss()
-                    }
-                }
-                dialog.show()
-                true
-            }
-            R.id.pageSearch -> {
-                val edit = EditText(this).apply {
-                    inputType = InputType.TYPE_CLASS_NUMBER
-                }
 
-                val dialog = AlertDialog.Builder(this).apply {
-                    setTitle("페이지 이동")
-                    setMessage("페이지를 입력하세요.")
-                    setView(edit)
-
-                    setPositiveButton("확인") { dialog, which ->
-                        val value = edit.text.toString().toInt() - 1
-                        val input = if (value < 0) 0
-                        else if (value >= textAdapter.itemCount) textAdapter.itemCount - 1
-                        else value
-
-                        val layoutManager =
-                            binding.recyclerView.layoutManager as LinearLayoutManager
-
-                        layoutManager.scrollToPositionWithOffset(input, 0)
-                    }
                     setNegativeButton("취소") { dialog, which ->
                         dialog.dismiss()
                     }
@@ -171,93 +154,122 @@ class MainActivity : AppCompatActivity() {
                 dialog.show()
                 true
             }
-            R.id.search -> {
-                true
+                R.id.pageSearch -> {
+                    val edit = EditText(this).apply {
+                        inputType = InputType.TYPE_CLASS_NUMBER
+                    }
+
+                    val dialog = AlertDialog.Builder(this).apply {
+                        setTitle("페이지 이동")
+                        setMessage("페이지를 입력하세요.")
+                        setView(edit)
+
+                        setPositiveButton("확인") { dialog, which ->
+                            val value = edit.text.toString().toInt() - 1
+                            val input = if (value < 0) 0
+                            else if (value >= textAdapter.itemCount) textAdapter.itemCount - 1
+                            else value
+
+                            val layoutManager =
+                                binding.recyclerView.layoutManager as LinearLayoutManager
+
+                            layoutManager.scrollToPositionWithOffset(input, 0)
+                        }
+                        setNegativeButton("취소") { dialog, which ->
+                            dialog.dismiss()
+                        }
+                    }
+
+                    dialog.show()
+                    true
+                }
+                R.id.search -> {
+                    true
+                }
+                R.id.fontSizeSetting -> {
+                    true
+                }
+                else -> super.onOptionsItemSelected(item)
             }
-            R.id.fontSizeSetting -> {
-                true
-            }
-            else -> super.onOptionsItemSelected(item)
         }
-    }
 
-    override fun onBackPressed() {
-        openFileExplorer()
-    }
+        override fun onBackPressed() {
+            openFileExplorer()
+        }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
+        override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+            super.onActivityResult(requestCode, resultCode, data)
 
-        if (resultCode == Activity.RESULT_OK) {
-            when (requestCode) {
-                OPEN_REQUEST_CODE -> {
-                    data?.let { it ->
-                        it.data?.let { it ->
-                            contentResolver.query(it, null, null, null, null)?.use { cursor ->
-                                val name = cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME)
-                                cursor.moveToFirst()
-                                title = cursor.getString(name)
+            if (resultCode == Activity.RESULT_OK) {
+                when (requestCode) {
+                    OPEN_REQUEST_CODE -> {
+                        data?.let { it ->
+                            it.data?.let { it ->
+                                contentResolver.query(it, null, null, null, null)?.use { cursor ->
+                                    val name = cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME)
+                                    cursor.moveToFirst()
+                                    title = cursor.getString(name)
+                                }
+
+                                val content = Filter().arrangement(readFile(it))
+
+                                initRecycler(content)
+
+                                binding.totalLine.text = textAdapter.itemCount.toString()
                             }
-
-                            val content = Filter().arrangement(readFile(it))
-
-                            initRecycler(content)
-
-                            binding.totalLine.text = textAdapter.itemCount.toString()
                         }
                     }
                 }
-            }
-        } else {
-            //아무런 파일도 선택되지 않았을 때 종료
-            finish()
-        }
-    }
-
-    private fun openFileExplorer() {
-        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
-            addCategory(Intent.CATEGORY_OPENABLE)
-            type = "text/plain"
-        }
-
-        startActivityForResult(intent, OPEN_REQUEST_CODE)
-    }
-
-    private fun readFile(uri: Uri): String {
-        val stringBuilder = StringBuilder()
-
-        try {
-            val inputStream = contentResolver.openInputStream(uri)
-            val reader = BufferedReader(InputStreamReader(inputStream))
-
-            while (true) {
-                val currentLine = reader.readLine() ?: break
-
-                stringBuilder.append(Filter.deleteGarbageText(currentLine) + "\n")
-            }
-
-            inputStream?.close()
-        } catch (e: IOException) {
-            e.printStackTrace()
-        }
-
-        val target: String = stringBuilder.toString()
-
-        return target.substring(0, target.length - 1) // 마지막 엔터 제외
-    }
-
-    private fun initRecycler(textList: ArrayList<String>) {
-        textAdapter = TextAdapter(this, binding.recyclerView.height)
-        binding.recyclerView.adapter = textAdapter
-
-        data.apply {
-            clear()
-            for (i in textList) {
-                add(i)
+            } else {
+                //아무런 파일도 선택되지 않았을 때 종료
+                finish()
             }
         }
 
-        textAdapter.data = data
-        textAdapter.notifyDataSetChanged()
+        private fun openFileExplorer() {
+            val intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
+                addCategory(Intent.CATEGORY_OPENABLE)
+                type = "text/plain"
+            }
+
+            startActivityForResult(intent, OPEN_REQUEST_CODE)
+        }
+
+        private fun readFile(uri: Uri): String {
+            val stringBuilder = StringBuilder()
+
+            try {
+                val inputStream = contentResolver.openInputStream(uri)
+                val reader = BufferedReader(InputStreamReader(inputStream))
+
+                while (true) {
+                    val currentLine = reader.readLine() ?: break
+
+                    stringBuilder.append(Filter.deleteGarbageText(currentLine) + "\n")
+                }
+
+                inputStream?.close()
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+
+            val target: String = stringBuilder.toString()
+
+            return target.substring(0, target.length - 1) // 마지막 엔터 제외
+        }
+
+        private fun initRecycler(textList: ArrayList<String>) {
+            textAdapter = TextAdapter(this, binding.recyclerView.height)
+            binding.recyclerView.adapter = textAdapter
+
+            data.apply {
+                clear()
+                for (i in textList) {
+                    add(i)
+                }
+            }
+
+            textAdapter.data = data
+            textAdapter.notifyDataSetChanged()
+        }
     }
-}
