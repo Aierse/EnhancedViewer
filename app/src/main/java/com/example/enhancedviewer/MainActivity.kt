6bbfc9ca801d417927e87e78706b250569d11e67
Aierse <dbsgur2008@gmail.com>
2022-06-12 17:01:46 +0900

    기능 - 북마크 Ui 출력

--- app/src/main/java/com/example/enhancedviewer/MainActivity.kt
+++ app/src/main/java/com/example/enhancedviewer/MainActivity.kt
@@ -2,19 +2,18 @@ package com.example.enhancedviewer
 
 import android.app.Activity
 import android.app.AlertDialog
+import android.content.DialogInterface
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.provider.OpenableColumns
 import android.text.InputType
-import android.text.TextUtils.split
-import android.util.Log
-import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 import android.view.MotionEvent
 import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.LinearSmoothScroller
 import androidx.recyclerview.widget.RecyclerView
@@ -65,7 +64,8 @@ class MainActivity : AppCompatActivity() {
 
                         if (center < e.y) {
                             // 증앙 아래 터치 시 아래로 한페이지 이동
-                            movement = recyclerViewLayoutManager.findLastCompletelyVisibleItemPosition() + 1
+                            movement =
+                                recyclerViewLayoutManager.findLastCompletelyVisibleItemPosition() + 1
                             smoothScroller = object : LinearSmoothScroller(baseContext) {
                                 override fun getVerticalSnapPreference(): Int {
                                     return SNAP_TO_START
@@ -73,7 +73,8 @@ class MainActivity : AppCompatActivity() {
                             }
                         } else {
                             // 중앙 위 터치 시 위로 한페이지 이동
-                            movement = recyclerViewLayoutManager.findFirstCompletelyVisibleItemPosition() - 1
+                            movement =
+                                recyclerViewLayoutManager.findFirstCompletelyVisibleItemPosition() - 1
                             smoothScroller = object : LinearSmoothScroller(baseContext) {
                                 override fun getVerticalSnapPreference(): Int {
                                     return SNAP_TO_END
@@ -117,11 +118,30 @@ class MainActivity : AppCompatActivity() {
                 val fileName = title.toString()
 
                 if (File("$filesDir/$fileName").exists()) {
-                    val bookmarkList = openFileInput(fileName).bufferedReader().lines().toList().map {
-                        Integer.parseInt(it)
+                    val bookmarkList =
+                        openFileInput(fileName).bufferedReader().lines().toList().map {
+                            Integer.parseInt(it)
+                        }
+
+                    val temp = arrayListOf<String>().apply {
+                        for (i in bookmarkList) {
+                            add(data[i])
+                        }
                     }
 
-                    //recyclerViewLayoutManager.scrollToPositionWithOffset(bookmarkList[2], 0)
+                    val bookmarkData = temp.toTypedArray()
+
+                    val dialog = AlertDialog.Builder(this).apply {
+                        setTitle("이동할 북마크를 선택하세요.")
+
+                        setItems(bookmarkData, DialogInterface.OnClickListener { dialog, id ->
+                            recyclerViewLayoutManager.scrollToPositionWithOffset(bookmarkList[id], 0)
+
+                            dialog.dismiss()
+                        })
+                    }
+
+                    dialog.show()
                 }
 
                 true
@@ -153,119 +173,119 @@ class MainActivity : AppCompatActivity() {
                 dialog.show()
                 true
             }
-                R.id.pageSearch -> {
-                    val edit = EditText(this).apply {
-                        inputType = InputType.TYPE_CLASS_NUMBER
+            R.id.pageSearch -> {
+                val edit = EditText(this).apply {
+                    inputType = InputType.TYPE_CLASS_NUMBER
+                }
+
+                val dialog = AlertDialog.Builder(this).apply {
+                    setTitle("페이지 이동")
+                    setMessage("페이지를 입력하세요.")
+                    setView(edit)
+
+                    setPositiveButton("확인") { dialog, which ->
+                        val value = edit.text.toString().toInt() - 1
+                        val input = if (value < 0) 0
+                        else if (value >= textAdapter.itemCount) textAdapter.itemCount - 1
+                        else value
+
+                        recyclerViewLayoutManager.scrollToPositionWithOffset(input, 0)
                     }
-
-                    val dialog = AlertDialog.Builder(this).apply {
-                        setTitle("페이지 이동")
-                        setMessage("페이지를 입력하세요.")
-                        setView(edit)
-
-                        setPositiveButton("확인") { dialog, which ->
-                            val value = edit.text.toString().toInt() - 1
-                            val input = if (value < 0) 0
-                            else if (value >= textAdapter.itemCount) textAdapter.itemCount - 1
-                            else value
-
-                            recyclerViewLayoutManager.scrollToPositionWithOffset(input, 0)
-                        }
-                        setNegativeButton("취소") { dialog, which ->
-                            dialog.dismiss()
-                        }
-                    }
-
-                    dialog.show()
-                    true
-                }
-                R.id.search -> {
-                    true
-                }
-                R.id.fontSizeSetting -> {
-                    true
-                }
-                else -> super.onOptionsItemSelected(item)
-            }
-        }
-
-        override fun onBackPressed() {
-            openFileExplorer()
-        }
-
-        override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-            super.onActivityResult(requestCode, resultCode, data)
-
-            if (resultCode == Activity.RESULT_OK) {
-                when (requestCode) {
-                    OPEN_REQUEST_CODE -> {
-                        data?.let { it ->
-                            it.data?.let { it ->
-                                contentResolver.query(it, null, null, null, null)?.use { cursor ->
-                                    val name = cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME)
-                                    cursor.moveToFirst()
-                                    title = cursor.getString(name)
-                                }
-
-                                val content = Filter().arrangement(readFile(it))
-
-                                initRecycler(content)
-
-                                binding.totalLine.text = textAdapter.itemCount.toString()
-                            }
-                        }
+                    setNegativeButton("취소") { dialog, which ->
+                        dialog.dismiss()
                     }
                 }
-            } else {
-                //아무런 파일도 선택되지 않았을 때 종료
-                finish()
+
+                dialog.show()
+                true
             }
-        }
-
-        private fun openFileExplorer() {
-            val intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
-                addCategory(Intent.CATEGORY_OPENABLE)
-                type = "text/plain"
+            R.id.search -> {
+                true
             }
-
-            startActivityForResult(intent, OPEN_REQUEST_CODE)
-        }
-
-        private fun readFile(uri: Uri): String {
-            val stringBuilder = StringBuilder()
-
-            try {
-                val inputStream = contentResolver.openInputStream(uri)
-                val reader = BufferedReader(InputStreamReader(inputStream))
-
-                while (true) {
-                    val currentLine = reader.readLine() ?: break
-
-                    stringBuilder.append(Filter.deleteGarbageText(currentLine) + "\n")
-                }
-
-                inputStream?.close()
-            } catch (e: IOException) {
-                e.printStackTrace()
+            R.id.fontSizeSetting -> {
+                true
             }
-
-            val target: String = stringBuilder.toString()
-
-            return target.substring(0, target.length - 1) // 마지막 엔터 제외
-        }
-
-        private fun initRecycler(textList: ArrayList<String>) {
-            textAdapter = TextAdapter(this, binding.recyclerView.height)
-            binding.recyclerView.adapter = textAdapter
-
-            data.apply {
-                clear()
-                for (i in textList) {
-                    add(i)
-                }
-            }
-
-            textAdapter.data = data
-            textAdapter.notifyDataSetChanged()
+            else -> super.onOptionsItemSelected(item)
         }
     }
+
+    override fun onBackPressed() {
+        openFileExplorer()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode == Activity.RESULT_OK) {
+            when (requestCode) {
+                OPEN_REQUEST_CODE -> {
+                    data?.let { it ->
+                        it.data?.let { it ->
+                            contentResolver.query(it, null, null, null, null)?.use { cursor ->
+                                val name = cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME)
+                                cursor.moveToFirst()
+                                title = cursor.getString(name)
+                            }
+
+                            val content = Filter().arrangement(readFile(it))
+
+                            initRecycler(content)
+
+                            binding.totalLine.text = textAdapter.itemCount.toString()
+                        }
+                    }
+                }
+            }
+        } else {
+            //아무런 파일도 선택되지 않았을 때 종료
+            finish()
+        }
+    }
+
+    private fun openFileExplorer() {
+        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
+            addCategory(Intent.CATEGORY_OPENABLE)
+            type = "text/plain"
+        }
+
+        startActivityForResult(intent, OPEN_REQUEST_CODE)
+    }
+
+    private fun readFile(uri: Uri): String {
+        val stringBuilder = StringBuilder()
+
+        try {
+            val inputStream = contentResolver.openInputStream(uri)
+            val reader = BufferedReader(InputStreamReader(inputStream))
+
+            while (true) {
+                val currentLine = reader.readLine() ?: break
+
+                stringBuilder.append(Filter.deleteGarbageText(currentLine) + "\n")
+            }
+
+            inputStream?.close()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+        val target: String = stringBuilder.toString()
+
+        return target.substring(0, target.length - 1) // 마지막 엔터 제외
+    }
+
+    private fun initRecycler(textList: ArrayList<String>) {
+        textAdapter = TextAdapter(this, binding.recyclerView.height)
+        binding.recyclerView.adapter = textAdapter
+
+        data.apply {
+            clear()
+            for (i in textList) {
+                add(i)
+            }
+        }
+
+        textAdapter.data = data
+        textAdapter.notifyDataSetChanged()
+    }
+}
